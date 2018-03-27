7cd50b4c2a89a494c2be8996cd884214a23817e3
niyasns <niyasns007@outlook.com>
2018-03-27 20:44:51 +0530

    Concurrent timer succesful

--- thread.py
+++ thread.py
@@ -11,7 +11,7 @@ class raspThread (threading.Thread):
     def run(self):
         print ("Staring recording with thread name " + self.name)
         threadLock.acquire()
-        print_time(self.name, self.counter, 1)
+        print_time(self.name, self.counter, 2)
         threadLock.release()
 
 def print_time(threadName, counter, delay):
@@ -20,12 +20,17 @@ def print_time(threadName, counter, delay):
         print("%s: %s" % (threadName, time.ctime(time.time())))
         counter -= 1
 
+def print_text():
+    print("10 seconds : ping")
+    threading.Timer(10, print_text).start()
+
 threadLock = threading.Lock()
 threads = []
 
-threadRecord = raspThread(1, "Record", 5)
+threadRecord = raspThread(1, "Record", 10)
 
 threadRecord.start()
+print_text()
 
 threads.append(threadRecord)
 
